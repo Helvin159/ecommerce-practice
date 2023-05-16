1f687725f423168eebd33b1311b7113bed05ad59
Helvin Rymer <helvinrymer@helvins-mbp.myfiosgateway.com>
2023-05-16 00:08:18 -0400

    adds temp shop page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 
 const App = () => {
+	const Shop = () => <h1>I am shop page</h1>;
+
 	return (
 		<Routes>
 			<Route path='/' element={<Navigation />}>
 				<Route index element={<Home />} />
-				<Route path='shop' element={null} />
+				<Route path='shop' element={<Shop />} />
 			</Route>
 		</Routes>
 	);

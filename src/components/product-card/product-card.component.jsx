12f822c15b0b8aa5809a12f3dd3f331a43bbb76e
Helvin Rymer <helvinrymer@helvins-mbp.myfiosgateway.com>
2023-05-17 12:15:13 -0400

    new updates, cart dropdown updates, adds quantity count to shopping bag, styles cart items, fixes html structure for cart dropdown, adds functions to context

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,7 +1,17 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import Button from '../button/button.component';
+
 import './product-card.styles.scss';
 
 const ProductCard = ({ product }) => {
+	const { addItemToCart } = useContext(CartContext);
+	const addProductToCart = () => {
+		addItemToCart(product);
+	};
+
 	if (product === (null || undefined)) return;
 
 	const { name, price, imageUrl } = product;
@@ -14,7 +24,9 @@ const ProductCard = ({ product }) => {
 				<span className='name'>{name}</span>
 				<span className='price'>{price}</span>
 			</div>
-			<Button buttonType='inverted'>Add to cart</Button>
+			<Button buttonType='inverted' onClick={addProductToCart}>
+				Add to cart
+			</Button>
 		</div>
 	);
 };

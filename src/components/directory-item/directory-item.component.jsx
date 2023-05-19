0bb4a0ed56147d5a6bef510950e1bdbcc0d6bac2
Helvin Rymer <helvinrymer@helvins-mbp.myfiosgateway.com>
2023-05-18 23:09:21 -0400

    updates

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
 	BackgroundImage,
 	Body,
@@ -13,8 +13,10 @@ const DirectoryItem = ({ category }) => {
 		<DirectoryItemContainer onClick={onNavigateHandler} key={id}>
 			<BackgroundImage imageurl={imageUrl} />
 			<Body>
-				<h2>{title}</h2>
-				<p>Shop now</p>
+				<h2 tabIndex={0}>{title}</h2>
+				<Link to={route} aria-label={title}>
+					Shop now
+				</Link>
 			</Body>
 		</DirectoryItemContainer>
 	);
